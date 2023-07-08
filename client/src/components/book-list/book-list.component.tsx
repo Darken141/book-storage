45aa73d789eeef3ddeb2c826b873561f41657a11
Darken141 <Darken141@gmail.com>
2023-07-08 12:42:27 +0200

    import EmptyList, Pagination, and search logic to the BookList

--- client/src/components/book-list/book-list.component.tsx
+++ client/src/components/book-list/book-list.component.tsx
@@ -1,43 +1,82 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import {
   removeBook,
   selectBook,
   clearSelectedBook,
+  addBook,
 } from '@/redux/features/book-storage.feature'
 import { setIsUpdating } from '@/redux/features/config.feature'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks.redux'
+import { searchAndHighlight, booksSeed } from '@/utils/book.utils'
 
 import { AnimatePresence } from 'framer-motion'
 import BookOverview from '@/components/book-overview/book-overview.component'
 import Modal from '@/components/modal/modal.component'
+import { EmptyList } from '@/components/empty-list/empty-list.component'
+import Pagination from '@/components/pagination/pagination.component'
 
 import styles from './book-list.module.css'
 
 export default function BookList() {
-  const { books, selectedBook } = useAppSelector(
+  const { books, selectedBook, searchQuery, numberToDisplay } = useAppSelector(
     (state) => state.bookStorageReducer
   )
   const dispatch = useAppDispatch()
   const [state, setState] = useState({
     showModal: false,
+    page: 1,
   })
 
   const onCloseModal = () => {
-    setState({ showModal: false })
+    setState((prevValue) => ({ ...prevValue, showModal: false }))
     dispatch(clearSelectedBook())
   }
+
+  const addSeedData = () => {
+    booksSeed.forEach((book) => {
+      dispatch(addBook(book))
+    })
+  }
+
+  const handlePageChange = (pageNumber: number) => {
+    console.log(pageNumber)
+    setState((prevValue) => ({ ...prevValue, page: pageNumber }))
+  }
+
+  const startPageSlice = useMemo(
+    () => (state.page - 1) * numberToDisplay,
+    [numberToDisplay, state.page]
+  )
+  const endPageSlice = useMemo(
+    () => state.page * numberToDisplay,
+    [numberToDisplay, state.page]
+  )
+
+  const booksToDisplay = searchQuery
+    ? searchAndHighlight(searchQuery, books).slice(0, numberToDisplay)
+    : books.slice(startPageSlice, endPageSlice)
+
+  useEffect(() => {
+    setState((prevValue) => ({ ...prevValue, page: 1 }))
+  }, [searchQuery, numberToDisplay])
 
   return (
     <>
       <div className={styles['list']}>
-        {books.map((book) => (
+        {booksToDisplay.length === 0 && (
+          <EmptyList
+            showAddButton={books.length === 0}
+            onAddClick={addSeedData}
+          />
+        )}
+        {booksToDisplay.map((book) => (
           <BookOverview
             key={book.id}
             book={book}
             onItemClick={(e, id) => {
-              setState({ showModal: true })
+              setState((prevValue) => ({ ...prevValue, showModal: true }))
               dispatch(selectBook(id))
             }}
             onDeleteClick={(e, id) => {
@@ -52,6 +91,15 @@ export default function BookList() {
           />
         ))}
       </div>
+
+      {!searchQuery && (
+        <Pagination
+          currentPage={state.page}
+          totalItems={books.length}
+          itemsPerPage={numberToDisplay}
+          onPageChange={handlePageChange}
+        />
+      )}
 
       <AnimatePresence>
         {state.showModal && selectedBook && (
