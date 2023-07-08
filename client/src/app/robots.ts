5fbacf40180f279235bf23c1a29d4c00b1f7264b
Darken141 <Darken141@gmail.com>
2023-07-08 13:37:01 +0200

    add robots & sitemap files

---
+++ client/src/app/robots.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const crawable: MetadataRoute.Robots = {
+    rules: {
+      userAgent: '*',
+      allow: '*',
+    },
+    sitemap: process.env.URL + '/sitemap.xml',
+  }
+
+  const notCrawable: MetadataRoute.Robots = {
+    rules: {
+      userAgent: '*',
+      disallow: '*',
+    },
+  }
+
+  return process.env.ENVIRONMENT === 'production' ? crawable : notCrawable
+}
