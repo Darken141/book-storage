36bc756859a364253df206193eb28f617c499cea
Darken141 <Darken141@gmail.com>
2023-07-08 12:36:02 +0200

    remove initial data, add searchQuery and numberToDisplay

--- client/src/redux/features/book-storage.feature.ts
+++ client/src/redux/features/book-storage.feature.ts
@@ -5,41 +5,15 @@ import { IBook } from '@/types/book.types'
 interface IBookStorage {
   books: IBook[]
   selectedBook: IBook | null
+  searchQuery: string
+  numberToDisplay: number
 }
 
 const initialState = {
-  books: [
-    {
-      id: '1',
-      title: 'The Lord of the Rings - The Fellowship of the Ring',
-      description:
-        'The Lord of the Rings is an epic high-fantasy novel written by English author and scholar J. R. R. Tolkien.',
-    },
-    {
-      id: '2',
-      title: 'The Hobbit - There and Back Again',
-      description:
-        'The Hobbit, or There and Back Again is a childrens fantasy novel by English author J. R. R. Tolkien.',
-    },
-    {
-      id: '3',
-      title: 'The Silmarillion',
-      description:
-        'The Silmarillion is a collection of mythopoeic works by English writer J. R. R. Tolkien, edited and published posthumously by his son, Christopher Tolkien, in 1977, with assistance from Guy Gavriel Kay.',
-    },
-    {
-      id: '4',
-      title: 'The Children of Húrin',
-      description:
-        'The Children of Húrin is an epic fantasy novel which forms the completion of a tale by J. R. R. Tolkien. He wrote the original version of the story in the late 1910s, revised it several times later, but did not complete it before his death in 1973.',
-    },
-    {
-      id: '5',
-      title: 'The Fall of Gondolin',
-      description:
-        'The Fall of Gondolin is, in the writings of J.R.R. Tolkien, one of the original Lost Tales which formed the basis for a section in his later work, The Silmarillion. A stand-alone, book-length version of the story was published on 30 August 2018.',
-    },
-  ],
+  books: [],
+  selectedBook: null,
+  searchQuery: '',
+  numberToDisplay: 10,
 } as IBookStorage
 
 export const bookStorage = createSlice({
@@ -47,6 +21,13 @@ export const bookStorage = createSlice({
   initialState,
   reducers: {
     reset: () => initialState,
+    setSearchQuery: (state, action: PayloadAction<string>) => {
+      state.searchQuery = action.payload
+    },
+    setNumberToDisplay: (state, action: PayloadAction<number>) => {
+      state.numberToDisplay = action.payload
+    },
+
     addBook: (state, action: PayloadAction<IBook>) => {
       state.books.push(action.payload)
     },
@@ -70,13 +51,15 @@ export const bookStorage = createSlice({
       state.books.sort((a, b) => a.title.localeCompare(b.title))
     },
     sortAlphabeticallyZA: (state) => {
-      state.books.sort((a, b) => b.title.localeCompare(a.title)).reverse()
+      state.books.sort((a, b) => b.title.localeCompare(a.title))
     },
   },
 })
 
 export const {
   addBook,
+  setSearchQuery,
+  setNumberToDisplay,
   updateBook,
   removeBook,
   selectBook,
